303441b8771a0d3e55ec4c45006c1ca175001b26
Robson <robsong200@gmail.com>
2020-10-23 20:50:37 -0300

    Iniciando a pagina search

--- github-user-search/src/pages/Search/index.tsx
+++ github-user-search/src/pages/Search/index.tsx
@@ -1,9 +1,21 @@
+import Button from 'core/components/Button';
 import React from 'react';
 import './styles.scss';
 
 const Search = () => (
-    <div>
-        <h1>Search</h1>
+    <div className="search-container">
+        <div className="search-content card-base border-radius-16">
+            <h1 className="search-text-title">Encontre um perfil Github</h1>
+            <input
+                //value={formData.name}
+                name="name"
+                type="text"
+                className="search-input"
+                //onChange={handleOnChange}
+                placeholder="Usuário Github"
+            />
+            <Button text="Encontrar"/>
+        </div>
     </div>
 );
 
