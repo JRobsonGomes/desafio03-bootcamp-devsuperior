f57861cca4b01411683712548c36ba2c4a1b1815
Robson <robsong200@gmail.com>
2020-10-25 15:05:50 -0300

    Melhorando o loader e estilos

--- github-user-search/src/pages/Search/components/SearchLoader/ImageLoader.tsx
+++ github-user-search/src/pages/Search/components/SearchLoader/ImageLoader.tsx
@@ -10,8 +10,8 @@ const ImageLoader = () => (
     backgroundColor="#dedede"
     foregroundColor="#f5f5f5"
   >
-    <rect x="19" y="23" rx="2" ry="2" width="284" height="284" /> 
-    <rect x="23" y="319" rx="0" ry="0" width="155" height="48" />
+    <rect x="0" y="23" rx="6" ry="6" width="284" height="284" /> 
+    <rect x="0" y="319" rx="6" ry="6" width="155" height="48" />
   </ContentLoader>
 )
 
